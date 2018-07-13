0e7822be2aeb0033df65d57a17d0a8e4b2c38900
Philip Yuen <philipyuen@cmail.carleton.ca>
2018-07-12 21:37:07 -0400

    Part 1-2 of Brackeys' 3D Tower Defense tutorial
    Part 1: Creation of the foundation of the map.
    Part 2: Creation of an enemy and using waypoints to create AI pathfinding.

---
+++ 3D Tower Defense/Assets/Enemy.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    public float speed = 5.5f;
+
+    private Transform target;
+    private int waypointIndex = 0;
+
+    private void Start() {
+        target = Waypoints.waypoints[0];
+    }
+
+    private void Update() {
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+
+        if (Vector3.Distance(transform.position, target.position) <= 0.3f) {
+            GetNextWaypoint();
+        }
+    }
+
+    void GetNextWaypoint() {
+
+        if (waypointIndex >= Waypoints.waypoints.Length - 1) {
+            Destroy(gameObject);
+            return;
+        }
+
+        waypointIndex++;
+        target = Waypoints.waypoints[waypointIndex];
+    }
+}
