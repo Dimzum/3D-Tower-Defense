ef54c45b0a5c101b1efe1a1c4626aa8d5b9fa42a
Philip Yuen <philipyuen@cmail.carleton.ca>
2018-07-14 23:23:57 -0400

    Part 3-4 of Brackeys' 3D Tower Defense tutorial
    Part 3: Created a wave spawn system to spawn enemies in waves, spawning more enemies the the higher the wave index.
    
    Part 4: Created turrets that target the nearest enemy. Turrets are not yet able to shoot.

---
+++ 3D Tower Defense/Assets/Scripts/Turret.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour {
+
+    private Transform _target;
+    public float range = 5f;
+
+    public string enemyTag = "Enemy";
+
+    public Transform partToRotate;
+    public float turnSpeed = 10f;
+
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("UpdateTarget", 0f, 0.5f);
+	}
+
+    void UpdateTarget() {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
+        float shortestDistance = Mathf.Infinity;
+        GameObject nearestEnemy = null;
+
+        foreach (GameObject enemy in enemies) {
+            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+            if (distanceToEnemy < shortestDistance) {
+                shortestDistance = distanceToEnemy;
+                nearestEnemy = enemy;
+            }
+        }
+
+        if (nearestEnemy != null && shortestDistance <= range) {
+            _target = nearestEnemy.transform;
+        } else {
+            _target = null;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if (_target == null) { return; }
+
+        // Target lock on
+        Vector3 dir = _target.position - transform.position;
+        Quaternion lookRotation = Quaternion.LookRotation(dir);
+        // Lerp smoothes the rotation (helps when changing to a new target)
+        Vector3 rotation = Quaternion.Lerp(partToRotate.rotation, lookRotation, Time.deltaTime * turnSpeed).eulerAngles;
+        partToRotate.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+	}
+
+    private void OnDrawGizmosSelected() {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
+}
---
+++ 3D Tower Defense/Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WaveSpawner : MonoBehaviour {
+
+    public Transform enemyPrefab;
+    public Transform spawnPoint;
+
+    public float timeBetweenWaves = 15.5f;
+    private float _countdown = 2.5f;
+    private float spawnDelay = 0.75f;
+
+    public Text waveCountdownText;
+    
+    private int _waveIndex = 0;
+
+    private void Update() {
+        if (_countdown <= 0f) {
+            StartCoroutine(SpawnWave());
+            _countdown = timeBetweenWaves;
+        }
+
+        _countdown -= Time.deltaTime;
+
+        waveCountdownText.text = Mathf.Floor(_countdown).ToString();
+    }
+
+    IEnumerator SpawnWave() {
+        _waveIndex++;
+
+        //for (int i = 0; i < Mathf.Pow(_waveIndex, (_waveIndex - 1)); i++) {
+        for (int i = 0; i < _waveIndex; i++) {
+            SpawnEnemy();
+            yield return new WaitForSeconds(spawnDelay);
+        }
+    }
+
+    void SpawnEnemy() {
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+}
